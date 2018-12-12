ffed215c2d2abd12f5aa90bbaef67e98c68026e3
Vasilis Koukoutis <vkoukoutis@hotmail.com>
2018-12-12 23:49:09 +0200

    - ApolloProvider refactored due to authorization headers, will be needed in the future for authentication. & more. - New packages for Apollo have been installed. - Login small changes. Handling error added.

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -17,16 +17,16 @@ const SIGNIN_MUTATION = gql`
 
 export default {
   name: "Login",
-  data: function() {
+  data() {
     return {
-      //test data
-      email: "vkouk@gmail.com",
-      password: "vaskouk"
+      email: "",
+      password: "",
+      error: ""
     };
   },
   methods: {
-    onLogin() {
-      this.$apolloProvider
+    onSubmit() {
+      this.$apolloProvider.defaultClient
         .mutate({
           mutation: SIGNIN_MUTATION,
           variables: {
@@ -38,6 +38,9 @@ export default {
           const token = data.data.signin.token;
           localStorage.setItem("token", token);
           router.push("/");
+        })
+        .catch(error => {
+          this.error = error.message.replace("GraphQL error:", "");
         });
     }
   }
@@ -45,5 +48,35 @@ export default {
 </script>
 
 <template>
-  <button @click="onLogin">Login</button>
+  <div class="form">
+    <form @submit.prevent="onSubmit" class="form__container">
+      <input type="text" v-model="email" class="form__input">
+      <input type="password" v-model="password" class="form__input">
+      <button @click="onSubmit" class="form__btn">Login</button>
+      <p class="form__error">{{error}}</p>
+    </form>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.form {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &__container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  &__input {
+    margin: 10px auto;
+    padding: 5px 25px;
+  }
+  &__error {
+    color: #de1212;
+    font-size: 14px;
+    font-weight: 700;
+    letter-spacing: 0.5px;
+  }
+}
+</style>
--- src/utils/Apollo.js
+++ src/utils/Apollo.js
@@ -1,12 +1,36 @@
 import Vue from "vue";
 import VueApollo from "vue-apollo";
-import ApolloClient from "apollo-boost";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { setContext } from "apollo-link-context";
+import { InMemoryCache } from "apollo-cache-inmemory";
 import { Config } from "./Config";
 
 Vue.use(VueApollo);
 
-const apolloProvider = new ApolloClient({
+const httpLink = new HttpLink({
   uri: Config.apolloUri
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : ""
+    }
+  };
+});
+
+const cache = new InMemoryCache();
+
+const apolloClient = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache
+});
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
 });
 
 export default apolloProvider;
